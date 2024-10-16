638bd05dd9a9bf50b626c9f9a06a731d7c32f233
Santiago Coria <santiago_coria@hotmail.com>
2024-10-16 18:32:07 -0300

    Queue

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,10 +1,14 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Playlist {
 
     /**
      * Constructor.
      */
+    private Queue<Song> nextSong;
     public Playlist() {
+       nextSong = new LinkedList<>();
 
     }
 
@@ -14,14 +18,20 @@ public class Playlist {
      * @return The next Song in the playlist, or null if the playlist is empty.
      */
     public Song getNextSong() {
-        return null;
-    }
+        if ( !nextSong.isEmpty()) {
+            return nextSong.remove();
 
+        } else {
+            return null;
+        }
+    }
     /**
      * Adds a Song to the end of the playlist.
      * @param song the song to be added to the playlist.
      */
     public void addSong(Song song) {
+        nextSong.add(song);
+
 
     }
 
